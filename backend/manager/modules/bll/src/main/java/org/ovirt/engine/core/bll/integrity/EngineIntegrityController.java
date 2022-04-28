a7bc7e267f387bebee343dd4f28fbca0ed362933
gilwoong-kang <smostr@gmail.com>
2022-04-28 10:27:39 +0900

    aide parse test

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/integrity/EngineIntegrityController.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/integrity/EngineIntegrityController.java
@@ -100,8 +100,8 @@ public class EngineIntegrityController extends AuditLogableBase {
     public String runAdminReq(){
         logger.info("run admin req...");
         String[] report = aide.aideRunWithLog().split("\n");
-        String vdsm = iVdsServer.runIntegrity().result;
-        logger.info(vdsm);
+//        String vdsm = iVdsServer.runIntegrity().result;
+//        logger.info(vdsm);
         if(isIntegrityFail(report)){
             StringBuilder log = new StringBuilder();
             for(String s : report){
