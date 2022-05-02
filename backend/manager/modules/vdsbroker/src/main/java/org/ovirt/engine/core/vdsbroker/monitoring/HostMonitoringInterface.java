e709f2bbdae70d9c3c28937ea15cb22243c92a18
gilwoong-kang <smostr@gmail.com>
2022-05-02 10:07:23 +0900

    host monitoring rollback

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/monitoring/HostMonitoringInterface.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/monitoring/HostMonitoringInterface.java
@@ -3,5 +3,4 @@ package org.ovirt.engine.core.vdsbroker.monitoring;
 public interface HostMonitoringInterface {
     void refresh();
     void afterRefreshTreatment();
-    void checkVdsIntegrity();
 }
